41d7e70da2faf3382db5734d1c69b4a79cd08fd7
indiealexh <champhaggy@gmail.com>
2022-03-04 08:13:43 -0800

    6 files modified

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# angular-tvcdcs
+# cnprc-ng-test
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tvcdcs)
+To complete this task you must:
+
+Read and follow the instructions in the code found at src/app/app.component.task
+
+If running in stack blitz, the page should refresh automatically when the save your work.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+/**
+ * Using the provided documentation, retrive the name value from the JSON file 'assets/data.json'
+ * And use it to replace the world property/variable
+ *
+ * https://angular.io/tutorial/toh-pt6#heroes-and-http
+ */
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  jsonUrl: string = 'assets/data.json';
+
   world: string = 'world';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // This runs on component load
+  }
 }
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
+class Data {
+  name: string;
+}
